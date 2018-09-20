63c73cb3ee1f9128fe4b538dde7f8dbdd1efd848
Jorge Niño Cabal <a01172309@itesm.mx>
2018-09-19 22:16:05 -0500

    MYSQL Configured

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -80,8 +80,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tsci',
+        'USER': 'root',
+        'PASSWORD':'Overflow953687',
+        'HOST': '127.0.0.1',
+        'PORT':'3306'
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
