7e1da8a38a3c91c708091151bb9151aeb5554e51
Jorge Niño Cabal <A01172309@itesm.mx>
2018-09-21 20:20:08 -0500

    All models created

--- mysite/noticias/models.py
+++ mysite/noticias/models.py
@@ -1,18 +1,11 @@
 from django.db import models
 
 # Create your models here.
-"""
-class Musician(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    instrument = models.CharField(max_length=100)
 
-class Album(models.Model):
-    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    release_date = models.DateField()
-    num_stars = models.IntegerField()
-"""
+# Table Community
+class Comunidad(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
 
 # Table Product
 class Producto(models.Model):
@@ -20,13 +13,18 @@ class Producto(models.Model):
     price=models.IntegerField()
     community = models.ForeignKey(Comunidad,on_delete=models.CASCADE)
 
-# Table Community
-class Comunidad(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
 
 # Table Notice
 class Notice(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     community = models.ForeignKey(Comunidad,on_delete=models.CASCADE)
+
+# Table Image
+class Image(models.Model):
+    path = models.CharField(max_length=255)
+
+# Table User
+class User(models.Model):
+    password=models.CharField(max_length=255)
+    name=models.CharField(max_length=150)
