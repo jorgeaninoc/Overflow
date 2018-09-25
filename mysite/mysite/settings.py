c86cc26e61ea31d5b0402f90bbdda09e0c726b07
Quique Posada <quiqueposadalozano@Quique-Posadas-MacBook-Pro.local>
2018-09-25 17:08:55 -0500

    Se_mejoro_la_consistencia_de_la_pagina

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tsci',
         'USER': 'root',
-        'PASSWORD':'Overflow953687',
+        'PASSWORD':'Formula10',
         'HOST': '127.0.0.1',
         'PORT':'3306'
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
