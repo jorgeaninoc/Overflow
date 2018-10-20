4bbbcfd58fd97173dad15259ad615bef6e91a7c3
MaritzaVIDEOJUEGOS <A01701357@itesm.mx>
2018-10-19 19:06:13 -0500

    Colabora

--- mysite/Colabora/tests.py
+++ mysite/Colabora/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 
-# Testing colabora displayed
+# Description: Testing colabora displayed
+# In charge: Maritza
+# Modify by Maritza
+# Modify date: 19 oct 18
 class ConsultColaboraTest(TestCase):
 
     def testConsultColabora(self):
