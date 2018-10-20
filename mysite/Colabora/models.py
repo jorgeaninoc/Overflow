915a2f6e470597a1117286c5d906970988745b18
Jorge Niño Cabal <A01172309@itesm.mx>
2018-10-19 19:23:49 -0500

    Add, Edit and Delete Colaborador and ColaboraImage finished

--- mysite/Colabora/models.py
+++ mysite/Colabora/models.py
@@ -1,16 +1,27 @@
+"""
+Created by Framework
+This file is where the models of Comunidades are declared.
+Modified by: Jorge Nino
+Date: 19/10/18
+"""
+# Import libraries used
 from django.db import models
 from datetime import timedelta as td
 from datetime import datetime as dt
 import django
 # Create your models here.
+# Create a model to save the image of Colabora site
 class ColaboraImagen(models.Model):
     nombre = models.CharField(max_length=255,unique=True,default=str(dt.now()))
     path = models.ImageField(upload_to="images")
 
+    # Function to return the name of the image
     def __str__(self):
         return self.nombre
-
+        
+    # Class to change super Class attributes
     class Meta:
+        # Change the verbose names to spanish language
         verbose_name: "Imagen"
         verbose_name_plural = "Imagenes"
 
--- mysite/Colabora/tests.py
+++ mysite/Colabora/tests.py
@@ -1,3 +1,9 @@
+"""
+Created by Framework
+This file is where the tests of Colabora are declared.
+Modified by: Jorge Nino
+Date: 19/10/18
+"""
 from django.test import TestCase
 
 # Create your tests here.
