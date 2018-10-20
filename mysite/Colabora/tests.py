1ee3a6fdbd8a79cbc86f53fb912ca88b27f708b5
MaritzaVIDEOJUEGOS <A01701357@itesm.mx>
2018-10-19 19:32:18 -0500

    Changes

--- mysite/Colabora/tests.py
+++ mysite/Colabora/tests.py
@@ -1,15 +1,11 @@
 """
 Created by Framework
 This file is where the tests of Colabora are declared.
-Modified by: Jorge Nino
+Modified by: Maritza
 Date: 19/10/18
 """
 from django.test import TestCase
 
-# Description: Testing colabora displayed
-# In charge: Maritza
-# Modify by Maritza
-# Modify date: 19 oct 18
 class ConsultColaboraTest(TestCase):
 
     def testConsultColabora(self):
