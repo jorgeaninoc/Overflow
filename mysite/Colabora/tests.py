f2ea5da9e8ab07fcf45ebd6a0e17989a159c83a1
Jorge Niño Cabal <A01172309@itesm.mx>
2018-10-24 16:57:59 -0500

    Fix tests

--- mysite/Colabora/tests.py
+++ mysite/Colabora/tests.py
@@ -4,7 +4,7 @@ This file is where the tests of Colabora are declared.
 Modified by: Maritza
 Modification date: 19/10/18
 """
-from django.test import TestCase
+from django.test import TestCase, Client
 
 class ConsultColaboraTest(TestCase):
 
