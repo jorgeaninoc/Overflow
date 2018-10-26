e4eeba10057cf6a20a07d874a6939910c8f51aff
Abraham <A01207538@itesm.mx>
2018-10-26 14:04:41 -0500

    consulta Somos Done

--- mysite/Somos/tests.py
+++ mysite/Somos/tests.py
@@ -4,7 +4,32 @@ This file is where you can create tests for the App
 Modified by: Jorge Nino
 Date: 19/10/18
 """
-# Import libraries needed
-from django.test import TestCase
+# Import libraries that will be used.
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+from Somos.models import *
+from Comunidades.models import Comunidad
+from datetime import datetime as dt
+from datetime import timedelta as td
+import django
 
-# Create your tests here.
+
+
+# Test for UC: Consult News
+class ConsultMissionTest(TestCase):
+    # Function that will be tested.
+    def testConsultMission(self):
+        # Get a client copy.
+        self.client = Client()
+        # Get the site from /actividades
+        response = self.client.get('/quiensomos')
+        # Check if the code return is 301 for success.
+        self.assertEqual(response.status_code, 301)
+
+    def testConsultMissionFalse(self):
+        # Get a client copy.
+        self.client = Client()
+        # Get the site from /actividades
+        response = self.client.get('/quiensomosZ')
+        # Check if the code return is 404 for failure
+        self.assertEqual(response.status_code, 404)
