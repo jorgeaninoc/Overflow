ac4ee99138fb7dabc909e4f3b3d0ef0f17f7b84b
MaritzaVIDEOJUEGOS <A01701357@itesm.mx>
2018-11-19 11:01:25 -0600

    Errors

--- mysite/Actividades/models.py
+++ mysite/Actividades/models.py
@@ -16,9 +16,8 @@ import django
 class Imagen(models.Model):
     #Declaring variables for Table image
     nombre = models.CharField(max_length=255,unique=True,default=str(dt.now()))
-    #Checking the vulnerability:
-    #path = models.ImageField(upload_to="images")
-    path = models.FileField(upload_to="images")
+    #Verifiying that the file is an image for vulnerability:
+    path = models.ImageField(upload_to="images")
 
     # Function to return the name of the Image
     def __str__(self):
