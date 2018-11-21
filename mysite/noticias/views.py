b594662fe7cdb4953bfd0408629338f85a904415
Abraham <A01207538@itesm.mx>
2018-11-21 12:44:47 -0600

    usability changes

---
+++ mysite/noticias/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+
+# Create your views here.
+def index(request):
+    return render(request,'noticias/index.html')
+
+def quiensomos(request):
+	return render(request, 'noticias/contactinformation.html');
+
+def noticias(request):
+	return render(request, 'noticias/noticias.html');
+
+def comunidades(request):
+	return render(request, 'noticias/comunidades.html');
+
+def colabora(request):
+	return render(request, 'noticias/contactinformation.html');
+
+def catalogo(request):
+	return render(request, 'noticias/contactinformation.html');
+
+def contactanos(request):
+	return render(request, 'noticias/contactinformation.html');
